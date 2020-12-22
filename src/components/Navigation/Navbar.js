f3eff4d46d64fff12a77582317cf4952b1ab643c
Samuel Gomez <samgj_18@hotmail.com>
2020-12-22 12:55:31 -0500

    [Feature]: Finishing user view

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light fixed-top'>
       <div className='container-fluid'>
         <Link className='navbar-brand' to='/'>
           Torre
@@ -36,17 +36,6 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          <form className='d-flex'>
-            <input
-              className='form-control me-2'
-              type='search'
-              placeholder='Search'
-              aria-label='Search'
-            />
-            <button className='btn btn-outline-primary' type='submit'>
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,28 @@
+import { Frame, Page } from 'framer';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import DownArrow from '../../assets/Icons/DownArrow';
-import { variants } from '../../helpers/Constants';
+import { container, variants } from '../../helpers/Constants';
 import { fetchUsername } from '../../helpers/Requests';
 import Card from '../Card/Card';
+import Location from '../Insights/Location';
+import Pie from '../Insights/Pie';
+import Radar from '../Insights/Radar';
+import './Ripple.css';
+
+/**
+ * A better approach would be use React Context to manage state instead of passing "search"
+ * prop throughout all the components. However, this is not a complicated statefull app, nor
+ * a tangled up one. Managing states in this way is fine. However it may be a good idea if I
+ * want to work with this is the future. Maybe using a different endpoint and another set of
+ * attributes.
+ */
 
 export default function Users() {
   const [delay, setDelay] = useState(1);
   const [render, setRender] = useState(false);
   const [search, setSearch] = useState(null);
+  const [change, setChange] = useState(false);
 
   useEffect(() => {
     const loaded = localStorage.getItem('loaded_users');
@@ -22,38 +36,43 @@ export default function Users() {
   return (
     render && (
       <div className='text-center mt-5'>
-        <motion.h1
-          initial='hidden'
-          animate='visible'
-          variants={variants}
-          transition={{ duration: 2 }}
-        >
-          Let's begin!,
-        </motion.h1>
-        <motion.h1
-          initial='hidden'
-          animate='visible'
-          variants={variants}
-          transition={{ delay: delay, duration: 2 }}
-        >
-          look for Torre's users
-        </motion.h1>
-        <motion.h1
-          initial='hidden'
-          animate='visible'
-          variants={variants}
-          transition={{ delay: delay * 2, duration: 2 }}
-        >
-          and find useful insights
-        </motion.h1>
-        <motion.div
-          initial='hidden'
-          animate='visible'
-          variants={variants}
-          transition={{ delay: delay * 3, duration: 2 }}
-        >
-          <DownArrow />
-        </motion.div>
+        {!change ? (
+          <>
+            <motion.h1
+              className='mt-5'
+              initial='hidden'
+              animate='visible'
+              variants={variants}
+              transition={{ duration: 2 }}
+            >
+              Let's begin!,
+            </motion.h1>
+            <motion.h1
+              initial='hidden'
+              animate='visible'
+              variants={variants}
+              transition={{ delay: delay, duration: 2 }}
+            >
+              look for Torre's users
+            </motion.h1>
+            <motion.h1
+              initial='hidden'
+              animate='visible'
+              variants={variants}
+              transition={{ delay: delay * 2, duration: 2 }}
+            >
+              and find useful insights
+            </motion.h1>
+            <motion.div
+              initial='hidden'
+              animate='visible'
+              variants={variants}
+              transition={{ delay: delay * 3, duration: 2 }}
+            >
+              <DownArrow />
+            </motion.div>
+          </>
+        ) : null}
         <motion.div
           initial='hidden'
           animate='visible'
@@ -62,18 +81,67 @@ export default function Users() {
           className='input-group mb-3 mt-4'
         >
           <input
+            autoComplete='off'
+            autoCorrect='off'
+            autoCapitalize='off'
+            spellCheck='false'
             type='text'
-            className='form-control'
+            className={`form-control ${change ? 'mt-5' : null}`}
             onChange={async e => {
               try {
                 setSearch(await fetchUsername(e.target.value));
+                setChange(true);
               } catch (error) {
                 setSearch(null);
               }
             }}
           />
         </motion.div>
-        {search !== null ? <Card info={search} /> : null}
+        {search !== null ? (
+          <>
+            <motion.h1
+              initial={{ scale: 2 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className='mt-4 mb-4'
+            >
+              {search.person.professionalHeadline !== undefined
+                ? search.person.professionalHeadline
+                : ''}
+            </motion.h1>
+
+            <Card info={search} />
+            <div className='container-fluid d-flex justify-content-center mt-5'>
+              <Page width={'80%'} height={'100%'} className='slider'>
+                <Frame size={150} radius={30} background={'transparent'}>
+                  <Pie user={search} />
+                </Frame>
+                <Frame size={150} radius={30} background={'transparent'}>
+                  <Location location={search.person.location} />
+                </Frame>
+                <Frame size={150} radius={30} background={'transparent'}>
+                  <Radar strengths={search} />
+                </Frame>
+              </Page>
+            </div>
+          </>
+        ) : change ? (
+          <>
+            <motion.div
+              variants={variants}
+              initial='show'
+              animate='hidden'
+              className='lds-ripple'
+              transition={{ duration: 2 }}
+            >
+              <div></div>
+              <div></div>
+            </motion.div>
+            <motion.div variants={container} initial='hidden' animate='show'>
+              <h1>Not found, type a different user</h1>
+            </motion.div>
+          </>
+        ) : null}
       </div>
     )
   );
